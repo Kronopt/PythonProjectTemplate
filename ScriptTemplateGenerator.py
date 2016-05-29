c225bd5e3b48e3e32c7120665eff682b5bac84ad
Kronopt <Kronopt@users.noreply.github.com>
2016-05-29 01:46:49 +0100

    Finished up most of the code

--- ScriptTemplateGenerator.py
+++ ScriptTemplateGenerator.py
@@ -24,7 +24,7 @@ import datetime
 def codeTemplate():
    '''
    '''
-
+   
    scriptName = 'scriptTemplate.py'
    shebang = '#!/usr/bin/env python'
    coding = '# coding: utf-8'
@@ -34,38 +34,56 @@ def codeTemplate():
    howToRun = 'TO DO HOW TO RUN'
    requires = 'TO DO REQUIRES'
    ensures = 'TO DO ENSURES'
-   _author = ''
-   _credits = []
-   _version = '0.1'
-   _date = '' # DATETIME
-   _status = 'Production'
+   _author = "__author__ = ''"
+   _credits = "__credits__ = []"
+   _version = "__version__ = '0.1'"
+   _date = "__date__ = " + _dateDefine()
+   _status = "__status__ = 'Production'"
+
+   scriptLines = shebang + '\n' + coding + "\n\n'''\n" + title + '\n\n' + description + \
+                 '\n\n' + dependencies + '\n' + howToRun + '\n' + requires + '\n' + ensures + \
+                 "\n'''\n\n" + _author + '\n' + _credits + '\n' + _version + '\n' + _date + \
+                 '\n' + _status + '\n\n# imports' + '\n\n# code'
    
-   # Verifies if file with the same name exists in the same directory, to avoid overwriting
+   # Verifies if file already exists
    if os.path.isfile(os.path.join(os.getcwd(), scriptName)):
-      print 'Error: File named "' + scriptName + '" already exists"
+      print 'Error: File named "' + scriptName + '" already exists'
    
    else:
       try:
          with open(scriptName, 'wb') as script:
-            script.write('cenaaas')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-            
+            script.write(scriptLines)
+ 
       except IOError, e:
          print 'Error with "' + e.filename + '" file:'
          print e.strerror
+
+def _dateDefine():
+   '''
+   '''
+
+   date = datetime.datetime.now()
+
+   # _date verification
+   hour = dateVerification(date.hour)
+   minute = dateVerification(date.minute)
+   day = dateVerification(date.day)
+   month = dateVerification(date.month)
+
+   # '##:##h, ##/##/####'
+   return "'" + hour + ':' + minute + 'h, ' + day + '/' + month + '/' + str(date.year) + "'"
+
+def dateVerification(value):
+   '''
+   Used in _dateDefine()
+   '''
+
+   value = str(value)
+   
+   if len(value) == 1:
+      value = '0' + value
+
+   return value
 
 ##def test():
 ##    """ Testing Docstring"""
