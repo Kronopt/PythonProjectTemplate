03183daab76ce4d81d91694a99a1e91faaf4d7a0
Kronopt <Kronopt@users.noreply.github.com>
2016-06-09 01:42:22 +0100

    Reworked some code and added description
    Added script description.
    Added a test() function placeholder and the "if __name__ == '__main__'"
    bit.
    Reworked some of the code on codeTemplate().
    Fixed indentation

--- ScriptTemplateGenerator.py
+++ ScriptTemplateGenerator.py
@@ -3,14 +3,17 @@
 
 '''
 SCRIPT TEMPLATE GENERATOR
+Generates a python script skeleton
 
-TO DO DESCRIPTION
+DEPENDENCIES
+    Python 2.7
 
-TO DO DEPENDENCIES
-TO DO HOW TO RUN
-TO DO REQUIRES
-TO DO ENSURES
+HOW TO RUN
+    Double click script
 '''
+
+import os
+import datetime
 
 __author__ = 'Pedro HC David, https://github.com/Kronopt'
 __credits__ = ['Pedro HC David']
@@ -18,78 +21,75 @@ __version__ = '0.1'
 __date__ = '21:06h, 27/05/2016'
 __status__ = 'Production'
 
-import os
-import datetime
-
 def codeTemplate():
-   '''
-   '''
-   
-   scriptName = 'scriptTemplate.py'
-   shebang = '#!/usr/bin/env python'
-   coding = '# coding: utf-8'
-   title = 'TO DO SCRIPT NAME'
-   description = 'TO DO DESCRIPTION'
-   dependencies = 'TO DO DEPENDENCIES'
-   howToRun = 'TO DO HOW TO RUN'
-   requires = 'TO DO REQUIRES'
-   ensures = 'TO DO ENSURES'
-   _author = "__author__ = ''"
-   _credits = "__credits__ = []"
-   _version = "__version__ = '0.1'"
-   _date = "__date__ = " + _dateDefine()
-   _status = "__status__ = 'Production'"
+    '''
+    Main Script
+    Defines the body of the template script, checks for some possible errors
+    and creates the file
+    '''
 
-   scriptLines = shebang + '\n' + coding + "\n\n'''\n" + title + '\n\n' + description + \
-                 '\n\n' + dependencies + '\n' + howToRun + '\n' + requires + '\n' + ensures + \
-                 "\n'''\n\n" + _author + '\n' + _credits + '\n' + _version + '\n' + _date + \
-                 '\n' + _status + '\n\n# imports' + '\n\n# code'
+    scriptName = 'scriptTemplate.py'
+    shebang = '#!/usr/bin/env python'
+    coding = '# coding: utf-8'
+    title = 'TO DO SCRIPT NAME'
+    description = 'TO DO DESCRIPTION'
+    dependencies = 'TO DO DEPENDENCIES'
+    howToRun = 'TO DO HOW TO RUN'
+    requires = 'TO DO REQUIRES'
+    ensures = 'TO DO ENSURES'
+    author = "__author__ = ''"
+    creditsList = "__credits__ = []"
+    version = "__version__ = '0.1'"
+    date = "__date__ = " + dateDefine()
+    status = "__status__ = 'Production'"
+    defTest = "def test():\n    pass\n\nif __name__ == '__main__':\n    test()"
+
+    scriptLines = shebang + '\n' + coding + "\n\n'''\n" + title + '\n' + description + '\n\n' \
+                + dependencies + '\n' + howToRun + '\n' + requires + '\n' + ensures + "\n'''\n\n" \
+                + '#\n# imports\n#\n\n' + author + '\n' + creditsList + '\n' + version + '\n' \
+                + date + '\n' + status + '\n\n#\n# code\n#\n\n' + defTest
    
-   # Verifies if file already exists
-   if os.path.isfile(os.path.join(os.getcwd(), scriptName)):
-      print 'Error: File named "' + scriptName + '" already exists'
+    # Verifies if file already exists
+    if os.path.isfile(os.path.join(os.getcwd(), scriptName)):
+        print 'Error: File named "' + scriptName + '" already exists'
    
-   else:
-      try:
-         with open(scriptName, 'wb') as script:
-            script.write(scriptLines)
+    else:
+        try:
+            with open(scriptName, 'wb') as script:
+                script.write(scriptLines)
  
-      except IOError, e:
-         print 'Error with "' + e.filename + '" file:'
-         print e.strerror
+        except IOError, e:
+            print 'Error with "' + e.filename + '" file:'
+            print e.strerror
 
-def _dateDefine():
-   '''
-   '''
+def dateDefine():
+    '''
+    Returns today's date and time in the following format:
+    HH:MMh, dd/mm/yyyy
+    '''
 
-   date = datetime.datetime.now()
+    date = datetime.datetime.now()
 
-   # _date verification
-   hour = dateVerification(date.hour)
-   minute = dateVerification(date.minute)
-   day = dateVerification(date.day)
-   month = dateVerification(date.month)
+    # _date verification
+    hour = dateVerification(date.hour)
+    minute = dateVerification(date.minute)
+    day = dateVerification(date.day)
+    month = dateVerification(date.month)
 
-   # '##:##h, ##/##/####'
-   return "'" + hour + ':' + minute + 'h, ' + day + '/' + month + '/' + str(date.year) + "'"
+    # '##:##h, ##/##/####'
+    return "'" + hour + ':' + minute + 'h, ' + day + '/' + month + '/' + str(date.year) + "'"
 
 def dateVerification(value):
-   '''
-   Used in _dateDefine()
-   '''
+    '''
+    Adds a 0 if hour minute or second is between 0 and 9
+    Used in dateDefine()
+    '''
 
-   value = str(value)
+    value = str(value)
    
-   if len(value) == 1:
-      value = '0' + value
+    if len(value) == 1:
+        value = '0' + value
 
-   return value
-
-##def test():
-##    """ Testing Docstring"""
-##    pass
-##
-##if __name__=='__main__':
-##    test()
+    return value
 
 codeTemplate()
