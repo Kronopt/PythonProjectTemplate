cd2bae1c874675a448eb8d5572fcf3ea5faf88eb
Kronopt <Kronopt@users.noreply.github.com>
2018-12-18 00:41:13 +0000

    Add Main.py example

---
+++ Project/Main.py
@@ -0,0 +1,70 @@
+#!python
+# coding: utf-8
+
+"""
+PROJECT/SCRIPT NAME IN CAPS LOCK
+small project/script description in a single line
+
+Larger description
+in multiple lines
+if necessary
+
+DEPENDENCIES:
+    - Python version
+    - module dependency 1.3
+    - more module dependency 3.0.1
+
+HOW TO RUN:
+    Through the command line?
+    What are the arguments?
+    Just double click?
+"""
+
+#
+# imports
+#
+
+__author__ = ''
+__credits__ = []
+__version__ = '0.1'
+
+
+def main(arg1, arg_with_options="default"):
+    """
+    One line quick description of the function/class
+
+    More description may be added
+    in multiple lines
+    if necessary
+
+    Parameters
+    ----------
+    arg1 : type
+        Description of argument
+        in multiple lines
+        if necessary
+    arg_with_options : {'option1', 'option2'}, optional
+        Default to first option when optional
+
+    Returns
+    -------
+    name : type
+        Explanation of return value. Use multiple times in case of multiple return values
+
+    Raises
+    ------
+    ExceptionType
+        Why and when it happens
+
+    Examples
+    --------
+    (Written in doctest format)
+
+    >>> example = "Hello World!"
+    >>> print(example)
+    Hello World!
+    """
+
+
+if __name__ == '__main__':
+    main()
--- README.md
+++ README.md
@@ -3,3 +3,22 @@ Python Project Template/Skeleton
 
 The objective of this repository is to have all the files, folders and style conventions that are usualy part of a standard python project on a single location.
 Just copy the ```Project``` folder to start a new python project.
+
+
+## TODO
+* README
+* LICENSE
+* .gitignore
+* tests/
+* docs/
+* .circleci
+* requirements.txt
+* requirements-dev.txt
+* bin/
+* bin/\_\_init__.py
+* Procfile (heroku)
+* .pylintrc
+* setup.py
+* check github for other files to add
+* check other repos for stuff to put here
+* check my other projects for docstrings and code
